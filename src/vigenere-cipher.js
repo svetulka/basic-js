c8837e89aeeefb9dd93efba9626789c6aefbb54c
Svetlana Vrublevskaya <svetulkavrub@gmail.com>
2020-11-01 12:53:44 +0300

    feat: implement vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    // this.reverse = isReverse;
+  }
+  encrypt(message, key) {
+
+  }
+  decrypt(message, key) {
+
   }
 }
 
